41fdc7fb114432916e3974bc008d5d4c9526748a
Ben Ward <bw@ben-ward.net>
2016-09-05 21:55:03 +0100

    Added authentication link generation and response parsing

---
+++ src/Raven/AuthenticationResponse.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Net;
+
+namespace Raven
+{
+    public class AuthenticationResponse
+    {
+        public int Ver;
+        public HttpStatusCode Status;
+        public string Msg;
+        public string Issue;
+        public string Id;
+        public Uri Url;
+        public string Principal;
+        public string[] Ptags;
+        public string Auth;
+        public string Sso;
+        public string Life;
+        public string Params;
+        public string Kid;
+        public string Sig;
+    }
+}
---
+++ src/Raven/RavenClient.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+
+namespace Raven
+{
+    public class RavenClient
+    {
+        private readonly string _url;
+        private const string BaseUrl = "https://demo.raven.cam.ac.uk/auth/authenticate.html";
+
+        public RavenClient(string url)
+        {
+            _url = url;
+        }
+
+        public Uri AuthenticationUrl(string desc = "")
+        {
+            var queryString = Uri.EscapeUriString("?ver=3&url=" + _url + "&desc=" + desc);
+            return new Uri(BaseUrl + queryString);
+        }
+
+        public AuthenticationResponse ParseResponse(string response)
+        {
+            var parameters = response.Split('!');
+            return new AuthenticationResponse
+            {
+                Ver = int.Parse(parameters[0]),
+                Status = (HttpStatusCode) int.Parse(parameters[1]),
+                Msg = parameters[2],
+                Issue = parameters[3],
+                Id = parameters[4],
+                Url = new Uri(Uri.UnescapeDataString(parameters[5])),
+                Principal = parameters[6],
+                Ptags = parameters[7].Split(','),
+                Auth = parameters[8],
+                Sso = parameters[9],
+                Life = parameters[10],
+                Params = parameters[11],
+                Kid = parameters[12],
+                Sig = parameters[13]
+            };
+        }
+    }
+}
